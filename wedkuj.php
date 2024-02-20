104572463ce8ac9ed5bd368196796a9372ef3771
Tiwit27 <lojekrafal27@gmail.com>
2024-02-20 05:27:37 +0100

    Praca zakończona w domu

---
+++ wedkuj.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Wędkowanie</title>
+    <link rel="stylesheet" href="styl_1.css">
+</head>
+<body>
+    <div id="baner">
+        <h1>Portal dla wędkarzy</h1>
+    </div>
+    <div id="lewy1">
+        <h3>Ryby zamieszkujące rzeki</h3>
+        <ol>
+            <?php
+              //skrypt 1
+            ?>
+        </ol>
+    </div>
+    <div id="prawy">
+        <img src="ryba1.jpg" alt="Sum">
+        <br>
+        <a href="kwerendy.txt">Pobierz kwerendy</a>
+    </div>
+    <div id="lewy2">
+        <h3>Ryby drapieżne naszych wód</h3>
+        <table>
+            <tr>
+                <th>L.p.</th>
+                <th>Gatunek</th>
+                <th>Występowanie</th>
+            </tr>
+            <?php
+                //skrypt 2
+             ?>
+        </table>
+    </div>
+    <div id="stopka">
+        <p>Stronę wykonał: PESEL</p>
+    </div>
+</body>
+</html>
